8372b87547e416f9cb916dd2bff5b88f05ea1e6b
Jordankwek <s10205052@connect.np.edu.sg>
2022-02-03 04:24:51 +0800

    Worked on the register and login of user

---
+++ MainAffinity/MainAffinity/ Controllers/ConversationViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ConversationViewController.swift
+//  MainAffinity
+//
+//  Created by Jordan Kwek on 3/2/22.
+//
+
+import UIKit
+
+class ConversationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .red
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let isLoggedIn = UserDefaults.standard.bool(forKey: "logged_in")
+        
+        if !isLoggedIn {
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+            present(nav, animated: false)
+        }
+    }
+
+}
