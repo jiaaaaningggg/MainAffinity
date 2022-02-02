e224db23fb72af1919de00480194791151f86520
jiaaaaningggg <s10205022@connect.np.edu.sg>
2022-02-02 20:05:54 +0800

    pushing walkthrough screen

--- MainAffinity/MainAffinity/ Controllers/ViewController.swift
+++ MainAffinity/MainAffinity/ Controllers/ViewController.swift
@@ -13,6 +13,17 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    override func viewDidAppear(_ animated: Bool) {
+        
+        if UserDefaults.standard.bool(forKey: "hasViewedWalkthrough") {
+            return
+        }
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        if let walkthroughViewController = storyboard.instantiateViewController(withIdentifier: "WalkthroughViewController") as? WalkthroughViewController {
+            present(walkthroughViewController, animated: true, completion: nil)
+        }
+    }
 
 
 }
