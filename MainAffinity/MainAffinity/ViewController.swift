f37a2e860becc18ceffcc9213a1b0ebf08f5fa05
jiaaaaningggg <73008985+jiaaaaningggg@users.noreply.github.com>
2022-02-02 16:52:52 +0800

    added files via upload

---
+++ MainAffinity/MainAffinity/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MainAffinity
+//
+//  Created by Jia Ning Tjong on 2/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
