f02896275b61e9bda7d0568b4929e14fa63cf3dc
Jordankwek <s10205052@connect.np.edu.sg>
2022-02-03 15:02:02 +0800

    Added chat function

---
+++ MainAffinity/MainAffinity/ Controllers/NewConversationViewController.swift
@@ -0,0 +1,62 @@
+//
+//  NewConversationViewController.swift
+//  MainAffinity
+//
+//  Created by Jordan Kwek on 3/2/22.
+//
+
+import UIKit
+import JGProgressHUD
+
+class NewConversationViewController: UIViewController {
+    
+    private let spinner = JGProgressHUD()
+    
+    private let searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.placeholder = "Search for Users..."
+        return searchBar
+    }()
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.isHidden = true
+        table.register(UITableViewCell.self,
+                       forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
+    private let noResultsLabel: UILabel = {
+        let label = UILabel()
+        label.isHidden = true
+        label.text = "No Results"
+        label.textAlignment = .center
+        label.textColor = .green
+        label.font = .systemFont(ofSize: 21, weight: .medium)
+        return label
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        navigationController?.navigationBar.topItem?.titleView = searchBar
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Cancel", style: .done, target: self, action: #selector(dismissSelf))
+        searchBar.becomeFirstResponder()
+    }
+    
+    //@objc so that function is available to the selector
+    @objc private func dismissSelf(){
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
+
+
+extension NewConversationViewController: UISearchBarDelegate{
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+    }
+    
+}
